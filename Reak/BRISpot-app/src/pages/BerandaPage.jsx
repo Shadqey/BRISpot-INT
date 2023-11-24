2f36cc63f4606b18e464d5ab2be8beb9b86c8721
Shadqey <shadqi.sadiya@gmail.com>
2023-11-24 14:31:43 +0700

    Added Memorandum App in Project

--- Reak/BRISpot-app/src/pages/BerandaPage.jsx
+++ Reak/BRISpot-app/src/pages/BerandaPage.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import NavigationComponents from '../components/NavigationComponents'
 
-const BerandaPage = ( {currentPage }) => {
-
+const BerandaPage = ({ currentPage }) => {
   return (
-    
-    <h1 className="text-3xl font-bold underline">
-        Ini Beranda
-    </h1>
-  )
-}
+    <div>
+      <div>
+        <h1 className="text-3xl font-bold underline"> Ini Beranda </h1>
+      </div>
+
+      <br></br>
+      
+      <h3>Nama</h3>
+      <h3>Personal Number</h3>
+      <h3>NIP</h3>
+      <h3>Jenis Kelamin</h3>
+      <h3>Eselon</h3>
+      <h3>Jabatan</h3>
+      <h3>Area Kerja</h3>
+      <h3>Sub Area Kerja</h3>
+      <h3>Unit Kerja</h3>
+      <h3>Level ID</h3>
+      <h3>Role Pengguna</h3>
+      <h3>E-mail</h3>
+      <h3>Nomor Handphone</h3>
+
+    </div>
+  );
+};
 
 export default BerandaPage
--- Reak/BRISpot-app/src/pages/PipelinePage.jsx
+++ Reak/BRISpot-app/src/pages/PipelinePage.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const PipelinePage = () => {
   return (
-    <div>PipelinePage</div>
+    <div>Pipeline Page</div>
   )
 }
 
