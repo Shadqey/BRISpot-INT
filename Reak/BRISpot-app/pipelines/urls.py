4494b159e8e2726da703377f06e78abeda4c80fa
Shadqey <shadqi.sadiya@gmail.com>
2023-11-27 09:56:22 +0700

    Created User Model

--- Reak/BRISpot-app/pipelines/urls.py
+++ Reak/BRISpot-app/pipelines/urls.py
@@ -4,5 +4,4 @@ from .views import *
 urlpatterns = [
     path('list/', PipelineView.as_view(), name = 'pipeline'),
     path('detail/<int:id>/', DetailPipelineView.as_view(), name = 'detail-pipeline'),
-    
 ]
