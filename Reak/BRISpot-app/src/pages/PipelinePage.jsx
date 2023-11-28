59d5730c05c508d72b8de267c5ee6d3dc1af1f1c
dyywann <ridwanefendy213@gmail.com>
2023-11-28 09:58:10 +0700

    Style table in pipeline page

--- Reak/BRISpot-app/src/pages/PipelinePage.jsx
+++ Reak/BRISpot-app/src/pages/PipelinePage.jsx
@@ -99,7 +99,7 @@ const PipelinePage = () => {
 
       <div class="overflow-x-auto shadow-md sm:rounded-lg my-6">
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <thead class="text-[14px] text-gray-700 uppercase bg-gray-50">
             <tr>
               <th scope="col" class="px-6 py-3">
                 No
@@ -125,42 +125,37 @@ const PipelinePage = () => {
             </tr>
           </thead>
           <tbody>
-            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+            <tr class="bg-white border-b">
               <th
                 scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                rowSpan={2}
               >
                 1
               </th>
-              <td class="px-6 py-4">Bank of Taiwan</td>
-              <td class="px-6 py-4">Bank of Taiwan Group</td>
-              <td class="px-6 py-4">$2999</td>
-              <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
+              <td class="px-6 py-4" rowSpan={3}>
+                Bank of Taiwan
               </td>
+              <td class="px-6 py-4" rowSpan={3}>
+                Bank of Taiwan Group
+              </td>
+              <td class="px-6 py-4">KI</td>
+              <td class="px-6 py-4">IDR</td>
+              <td class="px-6 py-4">2.000.000.000.000</td>
 
-              <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              <td class="px-6 py-4" rowSpan={3}>
+                <button
+                  type="button"
+                  class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Edit
-                </a>
+                  Default
+                </button>
               </td>
-
-              <td class="px-6 py-4">
-                <a
-                  href="#"
-                  class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Edit
-                </a>
-              </td>
+            </tr>
+            <tr class="bg-white">
+              <td class="px-6 py-4">BG</td>
+              <td class="px-6 py-4">IDR</td>
+              <td class="px-6 py-4">500.000.000.000</td>
             </tr>
           </tbody>
         </table>
