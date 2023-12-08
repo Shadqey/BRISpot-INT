cb43137f793b8d6d00f7b9e87be03d887da17b18
Shadqey <shadqi.sadiya@gmail.com>
2023-12-08 13:41:20 +0700

    Test Backend Homepage

--- SpringBoot/BRISpot/reak-v2/src/pages/BerandaPage.jsx
+++ SpringBoot/BRISpot/reak-v2/src/pages/BerandaPage.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavigationComponents from "../components/NavigationComponents";
 import { Typography } from "@material-tailwind/react";
 import { dataProfile } from "../data/index.js";
 import { Card } from "flowbite-react";
 
 const BerandaPage = ({ currentPage }) => {
+
+  const [userObject, setUserObject] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('api/users/90168000')
+      .then(response => response.json())
+      .then(data => {
+        setUserObject(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div>
       <Typography
@@ -13,16 +31,14 @@ const BerandaPage = ({ currentPage }) => {
       >
         Selamat Datang di Aplikasi BRISPOT WEB
       </Typography>
-
+      
       <Card>
-        {dataProfile.map((beranda) => {
           return (
             <div className="flex">
               <Typography
-                color="blue-gray"
-                className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]"
-              >
-                {beranda.nama}
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Nama
               </Typography>
 
               <Typography
@@ -36,11 +52,274 @@ const BerandaPage = ({ currentPage }) => {
                 color="blue-gray"
                 className="font-nunito font-normal text-Gray1 text-[16px]"
               >
-                {beranda.data}
+                {userObject.nama}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              PN
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.personalNumber}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              NIP
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.nip}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Jenis Kelamin
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.jenisKelamin}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Eselon
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.eselon}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Jabatan
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.jabatan}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Area Kerja
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.areaKerja}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Sub Area Kerja
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.subAreaKerja}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Unit Kerja
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.unitKerja}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Level ID
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.levelId}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Role Pengguna
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.rolePengguna}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Email
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.email}
+              </Typography>
+            </div>
+
+            <div className="flex">
+              <Typography
+              color="blue-gray"
+              className="font-nunito font-bold text-Gray1 text-[16px] w-[193px]">
+              Nomor Handphone
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-bold text-Gray1 text-[16px] pr-[24px]"
+              >
+                :
+              </Typography>
+
+              <Typography
+                color="blue-gray"
+                className="font-nunito font-normal text-Gray1 text-[16px]"
+              >
+                {userObject.nomorHandphone}
               </Typography>
             </div>
           );
-        })}
       </Card>
     </div>
   );
